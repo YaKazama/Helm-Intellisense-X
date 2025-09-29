0a9667e645fa42f0f49115093760350c7e36f540
Ya Kazama <kazamaya.y@gmail.com>
2025-09-29 12:44:19 +0800

    chore: 引入 JumpToConst 并生效 #1

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import { JumpToNamedTemplatesDefinitionProvider } from "./DefinitionProviders/helm/JumpToNamedTemplates";
 import { JumpToVariablesDefinitionProvider } from "./DefinitionProviders/helm/JumpToVariables";
+import { JumpToConstDefineProvider } from "./DefinitionProviders/helm/JumpToConst";
 import { JumpToValuesDefinitionProvider } from "./DefinitionProviders/yaml/JumpToValues";
 import { CapabilitiesCompletionItemProvider } from "./CompletionProviders/helm/Capabilities";
 import { ChartCompletionItemProvider } from "./CompletionProviders/helm/Chart";
@@ -22,6 +23,7 @@ export function activate(context: vscode.ExtensionContext) {
   vscode.languages.registerDefinitionProvider(helmLanguageActive, new JumpToNamedTemplatesDefinitionProvider())
   vscode.languages.registerDefinitionProvider(helmLanguageActive, new JumpToVariablesDefinitionProvider())
   vscode.languages.registerDefinitionProvider(helmLanguageActive, new JumpToValuesDefinitionProvider())
+  vscode.languages.registerDefinitionProvider(helmLanguageActive, new JumpToConstDefineProvider())
 
   vscode.languages.registerCompletionItemProvider(helmLanguageActive, new CapabilitiesCompletionItemProvider(), '.')
   vscode.languages.registerCompletionItemProvider(helmLanguageActive, new ChartCompletionItemProvider(), '.')
